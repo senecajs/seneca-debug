f148cb801c6b623c8c8598fdabaf8dba3d71d8cb
Vitor Lelis <vitorclelis96@gmail.com>
2022-12-06 17:11:37 -0300

    QOL changes

--- web/front/app.js
+++ web/front/app.js
@@ -1,17 +1,17 @@
 const top = {
   items: []
-};
+}
 
-var msgmap = {};
-var msgmapchildren = {};
-var msgmapdata = {};
-var searchlist = [];
+var msgmap = {}
+var msgmapchildren = {}
+var msgmapdata = {}
+var searchlist = []
 
-import * as d3 from 'd3';
-import * as d3flamegraph from 'd3-flame-graph';
-import * as d3tooltip from 'd3-flame-graph/src/tooltip';
-import VueJsonPretty from 'vue-json-pretty';
-import 'vue-json-pretty/lib/styles.css';
+import * as d3 from 'd3'
+import * as d3flamegraph from 'd3-flame-graph'
+import * as d3tooltip from 'd3-flame-graph/src/tooltip'
+import VueJsonPretty from 'vue-json-pretty'
+import 'vue-json-pretty/lib/styles.css'
 
 export default {
   components: {
@@ -22,10 +22,10 @@ export default {
       items: top.items,
       active: [],
       open: [],
-      search_txt: "",
-      filter_txt: "",
+      search_txt: '',
+      filter_txt: '',
       flamegraphdata: {
-        name: "root",
+        name: 'root',
         value: 0,
         children: []
       },
@@ -33,62 +33,62 @@ export default {
       toggleButtonMessage: 'Stop recording',
       allowChartUpdate: true,
       showDrilldownAlert: false,
-    };
+    }
   },
   created: function() {
-    const self = this;
-    this.$root.$on("msg", function(data) {
-      self.addmsg(data);
-    });
+    const self = this
+    this.$root.$on('msg', function(data) {
+      self.addmsg(data)
+    })
     this.$root.$on('flame', function(data) {
-      self.handleFlameChart(data);
-    });
+      self.handleFlameChart(data)
+    })
   },
   computed: {
     selected: function() {
-      if (!this.active.length) return undefined;
-      const id = this.active[0];
-      return msgmapdata[id];
+      if (!this.active.length) return undefined
+      const id = this.active[0]
+      return msgmapdata[id]
     }
   },
 
   watch: {
-    showDrilldownAlert(v) {
-      if (v) {
+    showDrilldownAlert(prevShowDrilldownAlertValue) {
+      if (prevShowDrilldownAlertValue) {
         setTimeout(() => {
-          this.showDrilldownAlert = false;
+          this.showDrilldownAlert = false
         }, 1000)
       }
     },
-    "items.length": function() {
-      if (this.search_txt) this.search();
+    'items.length': function() {
+      if (this.search_txt) this.search()
     },
     search_txt: function() {
-      this.search();
+      this.search()
     },
     filter_txt: function() {
-      this.$root.$emit("filter", this.filter_txt);
+      this.$root.$emit('filter', this.filter_txt)
     },
   },
   filters: {
     prettyJson(value) {
       if (!value) {
-        return '';
+        return ''
       }
-      return JSON.stringify(value, null, 2);
+      return JSON.stringify(value, null, 2)
     }
   },
 
   methods: {
     clear: function() {
-      searchlist = [];
-      msgmap = {};
-      msgmapchildren = {};
-      msgmapdata = {};
-      this.items.splice(0, this.items.length);
+      searchlist = []
+      msgmap = {}
+      msgmapchildren = {}
+      msgmapdata = {}
+      this.items.splice(0, this.items.length)
     },
     toggle() {
-      const active = this.toggleButtonMessage === 'Stop recording' ? false : true;
+      const active = this.toggleButtonMessage === 'Stop recording' ? false : true
       fetch(`${this.$root.expressBaseUrl}/toggle?active=${active}`, {
         method: 'POST'
       })
@@ -102,51 +102,51 @@ export default {
         .catch((err) => console.log('err', err))
     },
     load_children: function(data) {
-      data.children = msgmapchildren[data.id];
+      data.children = msgmapchildren[data.id]
     },
     addmsg: function(data) {
-      const meta = data.meta;
+      const meta = data.meta
       const parent = meta.parent
         ? meta.parent
         : meta.parents[0]
         ? meta.parents[0][1]
-        : null;
+        : null
 
-      if ("in" === data.debug_kind && !msgmap[meta.id]) {
+      if ('in' === data.debug_kind && !msgmap[meta.id]) {
         const parent_children = parent
           ? msgmapchildren[parent]
             ? msgmapchildren[parent]
             : top.items
-          : top.items;
+          : top.items
 
         const entry = {
           id: data.meta.id,
-          name: (data.meta.start % 10000000) + " " + data.meta.pattern,
+          name: (data.meta.start % 10000000) + ' ' + data.meta.pattern,
           children: [],
           error: false,
           duration: null,
           num_children: 0
-        };
+        }
 
         msgmapdata[meta.id] = {
           id: meta.id,
           name: data.name,
           data: data
-        };
+        }
 
-        parent_children.unshift(entry);
-        msgmap[meta.id] = entry;
-        msgmapchildren[meta.id] = [];
-      } else if ("out" === data.debug_kind && msgmap[meta.id]) {
-        msgmap[meta.id].error = data.error;
-        msgmap[meta.id].duration = meta.end - meta.start;
-        msgmap[meta.id].num_children = msgmapchildren[meta.id].length;
+        parent_children.unshift(entry)
+        msgmap[meta.id] = entry
+        msgmapchildren[meta.id] = []
+      } else if ('out' === data.debug_kind && msgmap[meta.id]) {
+        msgmap[meta.id].error = data.error
+        msgmap[meta.id].duration = meta.end - meta.start
+        msgmap[meta.id].num_children = msgmapchildren[meta.id].length
 
-        msgmapdata[meta.id].data.res = data.res;
-        msgmapdata[meta.id].data.err = data.err;
-        msgmapdata[meta.id].data.error = data.error;
-        msgmapdata[meta.id].data.meta.end = meta.end;
-        msgmapdata[meta.id].data.result_length = data.result_length;
+        msgmapdata[meta.id].data.res = data.res
+        msgmapdata[meta.id].data.err = data.err
+        msgmapdata[meta.id].data.error = data.error
+        msgmapdata[meta.id].data.meta.end = meta.end
+        msgmapdata[meta.id].data.result_length = data.result_length
 
         const searchListData = {
           id: meta.id,
@@ -154,87 +154,87 @@ export default {
           parent: parent
         }
 
-        searchlist.push(searchListData);
+        searchlist.push(searchListData)
       }
     },
     handleFlameChart(data) {
-      const { message } = data;
-      this.flamegraphdata = message;
-      this.buildChart();
+      const { message } = data
+      this.flamegraphdata = message
+      this.buildChart()
     },
     getIdsFromChartNode(node) {
-      const { data } = node;
-      const { _inner } = data;
-      const { ids } = _inner;
-      return ids;
+      return node.data._inner.ids
     },
     getMessageMapParentListFromId(id) {
-      let currentId = id;
-      const data = [currentId];
+      let currentId = id
+      const data = [currentId]
       const findParentById = (id) => {
         for (const parentId in msgmapchildren) {
           for (const children of msgmapchildren[parentId]) {
             if (children.id === id) {
-              return parentId;
+              return parentId
             }
           }
         }
-        return null;
+        return null
       }
-      while (true) {
-        const parentId = findParentById(currentId);
+      let counter = 0;
+      const MAXIMUM_LOOP_COUNTER = 1000000
+      while (counter < MAXIMUM_LOOP_COUNTER) {
+        const parentId = findParentById(currentId)
         if (!parentId) {
-          break;
+          break
         }
-        currentId = parentId;
-        data.push(currentId);
+        currentId = parentId
+        data.push(currentId)
+        counter++
       }
-      return data.reverse();
+      return data.reverse()
     },
     applyDrilldown(chartNode) {
-      const nodeIds = this.getIdsFromChartNode(chartNode);
+      const nodeIds = this.getIdsFromChartNode(chartNode)
       if (nodeIds.length) {
         nodeIds.forEach((nodeId) => {
-          const elem = this.getMessageMapParentListFromId(nodeId);
+          const elem = this.getMessageMapParentListFromId(nodeId)
 
-          const tree = this.$refs.msgtree.nodes;
-          const treeObjectKeyNames = Object.keys(tree);
+          const tree = this.$refs.msgtree.nodes
+          const treeObjectKeyNames = Object.keys(tree)
           for (const treeKey of treeObjectKeyNames) {
-            const node = tree[treeKey];
+            const node = tree[treeKey]
             if (!node) {
-              continue;
+              continue
             }
-            const vnodeEl = node.vnode.$el;
+            const vnodeEl = node.vnode.$el
             if (!elem.includes(treeKey)) {
-              vnodeEl.classList.add("hide")
+              vnodeEl.classList.add('hide')
             } else {
-              vnodeEl.classList.remove("hide")
+              vnodeEl.classList.remove('hide')
             }
           }
-        });
+        })
       } else {
-        const tree = this.$refs.msgtree.nodes;
-        const treeObjectKeyNames = Object.keys(tree);
+        const tree = this.$refs.msgtree.nodes
+        const treeObjectKeyNames = Object.keys(tree)
         for (const treeKey of treeObjectKeyNames) {
-          const node = tree[treeKey];
+          const node = tree[treeKey]
           if (!node) {
-            continue;
+            continue
           }
-          const vnodeEl = node.vnode.$el;
-          vnodeEl.classList.remove("hide")
+          const vnodeEl = node.vnode.$el
+          vnodeEl.classList.remove('hide')
         }
       }
-      this.showDrilldownAlert = true;
+      this.showDrilldownAlert = true
     },
     buildChart() {
       if(!this.flamegraphChart) {
-        const chart = d3flamegraph.flamegraph().width(screen.width * 0.90);
+        const chart = d3flamegraph.flamegraph().width(screen.width * 0.90)
 
         chart.label(d => {
-          const { data } = d;
-          const { name, value, _inner } = data;
+          const { data } = d
+          const { name, value, _inner } = data
           const { count } = _inner
-          return `Action name: ${name}  |  Average execution time: ${value}  |  Number of executions:${count}`;
+          return `Action name: ${name}  |  Average execution time: ${value}  |  Number of executions:${count}`
         })
         
         d3
@@ -243,16 +243,16 @@ export default {
           .call(chart)
 
         chart.onClick((node) => {
-          this.applyDrilldown(node);
+          this.applyDrilldown(node)
           
           if (node.data.name !== 'root') {
-            this.allowChartUpdate = false;
+            this.allowChartUpdate = false
           } else {
-            this.allowChartUpdate = true;
+            this.allowChartUpdate = true
           }
         })
         
-        this.flamegraphChart = chart;
+        this.flamegraphChart = chart
       } else {
         if (this.allowChartUpdate) {
           this.flamegraphChart.update(this.flamegraphdata)
@@ -261,47 +261,47 @@ export default {
     },
 
     search: function() {
-      var self = this;
-      self.term = this.search_txt.toLowerCase();
+      var self = this
+      self.term = this.search_txt.toLowerCase()
 
-      const list = self.$refs.msgtree.nodes;
+      const list = self.$refs.msgtree.nodes
 
       for (var i = 0; i < searchlist.length; i++) {
-        const item = searchlist[i];
-        const found = self.term && item.text.indexOf(self.term) !== -1;
+        const item = searchlist[i]
+        const found = self.term && item.text.indexOf(self.term) !== -1
 
         self.walkup(function(item) {
-          const node = list[item.id];
-          const vnode = node && node.vnode;
-          const el = vnode && vnode.$el;
+          const node = list[item.id]
+          const vnode = node && node.vnode
+          const el = vnode && vnode.$el
           if (el) {
             if (found) {
-              el.classList.add("found-msg");
+              el.classList.add('found-msg')
             } else {
-              el.classList.remove("found-msg");
+              el.classList.remove('found-msg')
             }
           }
-        }, item);
+        }, item)
       }
     },
 
     walk: function(op, item) {
       if (item) {
-        op(item);
+        op(item)
       }
 
-      const children = (item && item.children) || this.items;
+      const children = (item && item.children) || this.items
       for (var i = 0; i < children.length; i++) {
-        this.walk(op, children[i]);
+        this.walk(op, children[i])
       }
     },
     walkup: function(op, item, found) {
       if (item) {
-        op(item, found);
+        op(item, found)
       }
-      const parent = item && item.parent;
-      if (!parent) return;
-      this.walkup(op, searchlist[parent], found);
+      const parent = item && item.parent
+      if (!parent) return
+      this.walkup(op, searchlist[parent], found)
     }
   }
-};
+}
